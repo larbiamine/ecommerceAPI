8cabdfdf39f737bca9681f230c85e4efe5b22caf
larbiamine <larbiaminelj@gmail.com>
2022-11-10 14:18:28 +0100

    added logout and fixed redux

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/user");
 const authRoutes = require("./routes/auth");
@@ -11,19 +11,20 @@ const cors = require("cors");
 dotenv.config();
 const app = express();
 
-mongoose.connect(process.env.DATABASE_CONNECTION)
-.then(()=>console.log("Database Connected") )
-.catch((error) => console.log(error) ); 
+mongoose
+  .connect(process.env.DATABASE_CONNECTION)
+  .then(() => console.log("Database Connected"))
+  .catch((error) => console.log(error));
 
 app.use(express.json());
 app.use(cors());
-app.use("/api/auth", authRoutes );
-app.use("/api/user", userRoutes );
-app.use("/api/products", productRoutes );
-app.use("/api/cart", cartRoutes );
-app.use("/api/orders", orderRoutes );
-app.use("/api/checkout", stripeRoutes );
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/checkout", stripeRoutes);
 
-app.listen(process.env.PORT || 5000 , () => {
-    console.log("server running at port 5000");
-});  
+app.listen(process.env.PORT || 5000, () => {
+  console.log("server running at port 5000");
+});
