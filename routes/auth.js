817578816c6b51daee4a29a2435de3426ddbc623
larbiamine <larbiaminelj@gmail.com>
2022-12-26 20:43:57 +0100

    fixed jwt experation

--- routes/auth.js
+++ routes/auth.js
@@ -6,113 +6,129 @@ const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
-  try {
-    const filterUsername = { username: req.body.username };
-    const filterEmail = { email: req.body.email };
+	try {
+		const filterUsername = { username: req.body.username };
+		const filterEmail = { email: req.body.email };
 
-    const byUsername = await User.findOne(filterUsername);
-    const byEmail = await User.findOne(filterEmail);
+		const byUsername = await User.findOne(filterUsername);
+		const byEmail = await User.findOne(filterEmail);
 
-    if (byUsername) {
-      res.status(500).json("username already exists");
-      return;
-    }
-    if (byEmail) {
-      res.status(500).json("Email already exists");
-      return;
-    }
-  } catch (error) {
-    res.status(500).json(error);
-  }
+		if (byUsername) {
+			res.status(500).json("username already exists");
+			return;
+		}
+		if (byEmail) {
+			res.status(500).json("Email already exists");
+			return;
+		}
+	} catch (error) {
+		res.status(500).json(error);
+	}
 
-  const newUser = new User({
-    name: req.body.name,
-    lastname: req.body.lastname,
-    username: req.body.username,
-    email: req.body.email,
-    password: CryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.PASSWORD_SECRET_KEY
-    ).toString(),
-  });
-  try {
-    const savedUser = await newUser.save();
+	const newUser = new User({
+		name: req.body.name,
+		lastname: req.body.lastname,
+		username: req.body.username,
+		email: req.body.email,
+		password: CryptoJS.AES.encrypt(
+			req.body.password,
+			process.env.PASSWORD_SECRET_KEY
+		).toString(),
+	});
+	try {
+		const savedUser = await newUser.save();
 
-    //Creating users cart
-    const cart = {
-      userId: savedUser._id,
-      products: [],
-    };
-    const newCart = new Cart(cart);
-    await newCart.save();
+		//Creating users cart
+		const cart = {
+			userId: savedUser._id,
+			products: [],
+		};
+		const newCart = new Cart(cart);
+		await newCart.save();
 
-    //Creating User's Wishlist
-    const wishlist = {
-      userId: savedUser._id,
-      products: [],
-    };
-    const newWishlist = new Wishlist(wishlist);
-    await newWishlist.save();
-    res.status(201).json(savedUser);
-  } catch (error) {
-    res.status(500).json(error);
-  }
+		//Creating User's Wishlist
+		const wishlist = {
+			userId: savedUser._id,
+			products: [],
+		};
+		const newWishlist = new Wishlist(wishlist);
+		await newWishlist.save();
+		res.status(201).json(savedUser);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
 router.post("/login", async (req, res) => {
-  try {
-    const user = await User.findOne({ username: req.body.username });
+	try {
+		const user = await User.findOne({ username: req.body.username });
 
-    if (!user) {
-      res.status(401).json("wrong credentials (user not found)");
-      return;
-    }
+		if (!user) {
+			res.status(401).json("wrong credentials (user not found)");
+			return;
+		}
 
-    const hashpass = CryptoJS.AES.decrypt(
-      user.password,
-      process.env.PASSWORD_SECRET_KEY
-    );
-    const userPassword = hashpass.toString(CryptoJS.enc.Utf8);
+		const hashpass = CryptoJS.AES.decrypt(
+			user.password,
+			process.env.PASSWORD_SECRET_KEY
+		);
+		const userPassword = hashpass.toString(CryptoJS.enc.Utf8);
 
-    if (userPassword != req.body.password) {
-      res.status(401).json("wrong credentials");
-      return;
-    }
+		if (userPassword != req.body.password) {
+			res.status(401).json("wrong credentials");
+			return;
+		}
 
-    const accessToken = jwt.sign(
-      {
-        id: user._id,
-        isAdmin: user.isAdmin,
-      },
-      process.env.JWT,
-      { expiresIn: "3d" }
-    );
+		const accessToken = jwt.sign(
+			{
+				id: user._id,
+				isAdmin: user.isAdmin,
+			},
+			process.env.JWT,
+			{ expiresIn: "3d" }
+		);
 
-    const { password, ...others } = user._doc;
-    res.status(200).json({ ...others, accessToken });
-  } catch (error) {
-    res.status(500).json(error);
-  }
+		const { password, ...others } = user._doc;
+		res.status(200).json({ ...others, accessToken });
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
 router.get("/checktoken", (req, res) => {
-  const authHeader = req.headers.token;
-  if (authHeader) {
-    const token = authHeader.split(" ")[1];
-
-    jwt.verify(token, process.env.JWT, (err) => {
-      if (err) {
-        if (err.message === "jwt expired") {
-          return res.status(200).json("token expired");
-        } else {
-          res.status(403).json("token is not valid");
-        }
-      }
-      return res.status(200).json("token valid");
-    });
-  } else {
-    return res.status(401).json("You are Not authenticated");
-  }
+	const authHeader = req.headers.token;
+	if (authHeader) {
+		const token = authHeader.split(" ")[1];
+		try {
+			const ver = jwt.verify(token, process.env.JWT);
+			return res.status(200).json(ver);
+		} catch (err) {
+			if (err) {
+				if (err.message === "jwt expired") {
+					return res.status(200).json("token expired");
+				} else {
+					res.status(403).json("token is not valid");
+				}
+			}
+		}
+		// jwt
+		// 	.verify(token, process.env.JWT)
+		// 	.then((user) => {
+		// 		return res.status(200).json(user);
+		// 	})
+		// 	.catch((err) => {
+		// 		if (err) {
+		// 			if (err.message === "jwt expired") {
+		// 				return res.status(200).json("token expired");
+		// 			} else {
+		// 				res.status(403).json("token is not valid");
+		// 			}
+		// 		}
+		// 		return res.status(200).json("token valid");
+		// 	});
+	} else {
+		return res.status(401).json("You are Not authenticated");
+	}
 });
 
 module.exports = router;
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,58 +1,73 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.token;
+	const authHeader = req.headers.token;
 
-  if (authHeader) {
-    const token = authHeader.split(" ")[1];
+	if (authHeader) {
+		const token = authHeader.split(" ")[1];
+		try {
+			const user = jwt.verify(token, process.env.JWT);
 
-    jwt.verify(token, process.env.JWT, (err, user) => {
-      if (err) {
-        console.log(err);
-        if (err.message === "jwt expired") {
-          res.status(403).json("token expired");
-        } else {
-          res.status(403).json("token is not valid");
-        }
-        // res.status(403).json(err.message);
-      }
-      req.user = user;
-      next();
-    });
-  } else {
-    return res.status(401).json("You are Not authenticated");
-  }
+			req.user = user;
+			next();
+		} catch (err) {
+			console.log(err.message);
+			if (err.message === "jwt expired") {
+				// res.status(200).json("token expired");
+				res.status(200).json({ redirectURL: "/login" });
+				// res.redirect("/login");
+			} else {
+				res.status(200).json("token is not valid");
+			}
+		}
+
+		// jwt.verify(token, process.env.JWT, (err, user) => {
+		// 	if (err) {
+		// 		console.log(err);
+		// 		console.log(err.message);
+		// 		if (err.message === "jwt expired") {
+		// 			res.status(403).json("token expired");
+		// 		} else {
+		// 			res.status(403).json("token is not valid");
+		// 		}
+		// 	}
+		// 	req.user = user;
+		// 	next();
+		// });
+	} else {
+		return res.status(200).json("You are Not authenticated");
+	}
 };
 
 const verifyTokenAndAuthorization = (req, res, next) => {
-  verifyToken(req, res, () => {
-    if (req.user.id === req.params.id || req.user.isAdmin) {
-      next();
-    } else {
-      res.status(403).json("Not Allowed..");
-    }
-  });
+	verifyToken(req, res, () => {
+		if (req.user.id === req.params.id || req.user.isAdmin) {
+			next();
+		} else {
+			res.status(403).json("Not Allowed..");
+		}
+	});
 };
 
 const verifyTokenAndAdmin = (req, res, next) => {
-  verifyToken(req, res, () => {
-    if (req.user.isAdmin) {
-      next();
-    } else {
-      res.status(403).json("Not Allowed (not admin)");
-    }
-  });
+	verifyToken(req, res, () => {
+		if (req.user.isAdmin) {
+			next();
+		} else {
+			res.status(403).json("Not Allowed (not admin)");
+		}
+	});
 };
 
 const verifyTokenn = (req, res, next) => {
-  verifyToken(req, res, () => {
-    next();
-  });
+	verifyToken(req, res, () => {
+		next();
+	});
 };
 
 module.exports = {
-  verifyToken,
-  verifyTokenn,
-  verifyTokenAndAuthorization,
-  verifyTokenAndAdmin,
+	verifyToken,
+	verifyTokenn,
+	verifyTokenAndAuthorization,
+	verifyTokenAndAdmin,
 };
